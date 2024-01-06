e8fd2b8b33d7db2292def7382087fcd4d5b832c0
NoNotNotes <kmuy3733val@student.fatima.edu.ph>
2024-01-06 22:11:17 +0800

    new

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
+          Created by <strong>Kenneth</strong> with <a href="https://quartz.jzhao.xyz/">Quartz</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
