5d651e6cbdc35e2d57910648be60c43f52859a88
NoNotNotes <kmuy3733val@student.fatima.edu.ph>
2024-01-12 17:48:07 +0800

    layout update, B316/03, 05

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,14 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 2,
+        showTags: false
+      },
+      globalGraph: {
+      }
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
